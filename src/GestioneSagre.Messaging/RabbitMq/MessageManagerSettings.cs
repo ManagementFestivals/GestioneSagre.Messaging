ba662e24ad7213de7fc0db6bedce88f810047318
Angelo Pirola <angelo9871@gmail.com>
2023-02-21 21:49:26 +0100

    Aggiunta configurazione RabbitMQ

---
+++ src/GestioneSagre.Messaging/RabbitMq/MessageManagerSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GestioneSagre.Messaging.RabbitMq;
+
+public class MessageManagerSettings
+{
+    public string ConnectionString { get; set; }
+    public string ExchangeName { get; set; }
+    public ushort QueuePrefetchCount { get; set; }
+    public JsonSerializerOptions JsonSerializerOptions { get; set; } = JsonOptions.Default;
+}
