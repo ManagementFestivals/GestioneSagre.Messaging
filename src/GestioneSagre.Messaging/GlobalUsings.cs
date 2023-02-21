4b1cfd0f88193d605afc3ad4eea34f9eaac8dff4
Angelo Pirola <angelo9871@gmail.com>
2023-02-21 21:49:53 +0100

    Centralizzati usings

---
+++ src/GestioneSagre.Messaging/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using GestioneSagre.Messaging.Abstractions;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using RabbitMQ.Client;
+global using RabbitMQ.Client.Events;
